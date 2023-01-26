d99cc383de3ed4eaf9cfe726cefe62a1e1fa975b
Owen Ip <ipowen10@gmail.com>
2023-01-25 22:58:25 -0800

    Finish 1.8-1.11 unicafe

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -6,26 +6,60 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button buttonText={'good'} handleClick={() => setGood(good + 1)} />
+      <Button buttonText={'natural'} handleClick={() => setNeutral(neutral + 1)} />
+      <Button buttonText={'bad'} handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Button = ({ buttonText, handleClick }) => {
+  return (
+    <button onClick={handleClick}>{buttonText}</button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
+
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 
   const average = (good - bad) / total;
   const positive = good / total * 100;
 
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>natural</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
-    </div>
+    <table>
+      <thead>
+        <tr><th>Statistics</th></tr>
+      </thead>
+      <tbody>
+        <StatisticLine text={'good'} value={good} />
+        <StatisticLine text={'neutral'} value={neutral} />
+        <StatisticLine text={'bad'} value={bad} />
+        <StatisticLine text={'all'} value={total} />
+        <StatisticLine text={'average'} value={average} />
+        <StatisticLine text={'positive'} value={positive + '%'} />
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
