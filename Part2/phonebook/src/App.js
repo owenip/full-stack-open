cd899e1cad231c7903b98d09f5250ee8be58268e
Owen Ip <ipowen10@gmail.com>
2023-02-01 16:12:20 -0800

    Finish Ex 2.11

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import Phonebook from './Phonebook.js'
 import PersonForm from './PersonForm.js';
 import Filter from './Filter.js';
 
 const App = () => {
+    const dbUrl = 'http://localhost:3001/db';
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [nameFilter, setNameFilter] = useState('');
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([]);
 
+    const fetchDbHook = () => {
+        axios
+            .get(dbUrl)
+            .then(response => {
+                setPersons(response.data.persons)
+            })
+    }
 
     const handleSubmitPerson = (event) => {
         event.preventDefault();
@@ -58,6 +62,8 @@ const App = () => {
     const handleFilterChange = (event) => {
         setNameFilter(event.target.value);
     }
+
+    useEffect(fetchDbHook, []);
 
     return (
         <div>
