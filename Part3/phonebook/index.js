ed39010285e20e53cd3df01d516fc484b7077f5a
Owen Ip <ipowen10@gmail.com>
2023-02-19 19:11:56 -0800

    Finish 3.2

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -7,32 +7,39 @@ const PORT = 3001;
 app.use(express.json());
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ];
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
-})
+});
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} ${persons.length > 1 ? 'people' : 'person'}</p>
+        <p>${new Date()}</p>`
+    );
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+});
