4cc9c15357997690a83fe6c393a0fecc889c5a1b
Owen Ip <ipowen10@gmail.com>
2023-02-20 23:36:45 -0800

    Finish 3.7

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const express = require('express');
 const { response } = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
     {
