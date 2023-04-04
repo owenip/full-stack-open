193ffa3f00e033d53c4b10808dfad6bd25471c61
Owen Ip <ipowen10@gmail.com>
2023-04-04 10:18:44 -0700

    Finish 3.19-3.22

--- Part3/phonebook/models/person.js
+++ Part3/phonebook/models/person.js
@@ -4,15 +4,30 @@ const url = process.env.MONGODB_URI;
 mongoose
     .connect(url)
     .then(() => {
-        console.log('connected to MongoDB')
+        console.log('connected to MongoDB');
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
+        console.log('error connecting to MongoDB:', error.message);
     });
 
 const PersonSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /^(\d{8,})$|^(\d{2,3}-\d{5,})$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    },
 });
 
 PersonSchema.set('toJSON', {
