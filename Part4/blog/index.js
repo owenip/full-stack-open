2059db58f63a1339694c112f50f53bdaaf47e2cc
Owen Ip <ipowen10@gmail.com>
2023-04-09 20:26:44 -0700

    Add middleware for logging and error handling

--- Part4/blog/index.js
+++ Part4/blog/index.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import blogsRouter from './controllers/blog.js';
 
 import config from './utils/config.js';
-import { info, error } from './utils/logger.js';
+import * as logger from './utils/logger.js';
+import * as middleware from './utils/middleware.js';
 
 const app = express();
 
@@ -14,8 +15,13 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(json());
+app.use(middleware.requestLogger);
+
 app.use('/api/blogs', blogsRouter);
 
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
 app.listen(config.PORT, () => {
-    info(`Server running on port ${config.PORT}`);
+    logger.info(`Server running on port ${config.PORT}`);
 });
