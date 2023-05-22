57e7d4c886ac170c73f15028c65d595bf63e2473
Owen Ip <ipowen10@gmail.com>
2023-05-21 22:59:26 -0700

    Finish 4.5

--- Part4/blog/utils/list_helper.js
+++ Part4/blog/utils/list_helper.js
@@ -3,11 +3,21 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-    // return 0;
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((accum, blog) => {
+        if (!Object.prototype.hasOwnProperty.call(accum, 'likes') || blog.likes > accum.likes) {
+            return blog;
+        } else {
+            return accum;
+        }
+    }, {});
 };
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
 };
